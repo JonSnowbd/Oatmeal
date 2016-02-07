d80045b6b14bb28f7c053cdfe9e10b8a1612a25c
Josiah Parent <vasti009@gmail.com>
2016-02-06 22:18:37 -0400

    Added auto prefixing

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+var autoprefixer = require("autoprefixer");
+var precss       = require("precss");
+
 module.exports = {
 	entry: "./app/webpack/webpack.js",
 	output: {
@@ -6,14 +9,17 @@ module.exports = {
 	},
 	module: {
 		loaders: [
-			{test: /\.css$/, loader: "style!css"},
-			{test: /\.less$/, loader:"style!css!less"},
-         {test: /\.scss$/, loader:"style!css!sass"},
+			{test: /\.css$/, loader: "style!css!postcss"},
+			{test: /\.less$/, loader:"style!css!postcss!less"},
+         {test: /\.scss$/, loader:"style!css!postcss!sass"},
          {
             test: /\.js$/,
             exclude: /(node_modules|bower_components)/,
             loader:"babel?presets[]=es2015"
          },
 		]
-	}
+	},
+   postcss: function(){
+      return [autoprefixer, precss]
+   }
 }
